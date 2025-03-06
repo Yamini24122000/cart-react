22a667149140f5f9b5720a0e650d911dd72ca9a9
Yamini <yaminiy485@gmail.com>
2025-03-06 11:09:49 +0530

    Heyy

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>We Are Learning How to Install and Run React JS Project</p>
+      </header>
+    </div>
+  )
+}
+
+export default App;
